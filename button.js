f1a27d0af53854da33689ab6de5f8535104469e1
Tobias Lystad <tobias24@me.com>
2017-09-25 13:36:45 +0200

    Express server setup

--- button.js
+++ button.js
@@ -1,3 +1,15 @@
+// Import frameworks
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+app.use(express.static(__dirname + '/static'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
 var five = require("johnny-five"),
   board, button, button1, button2;
 
